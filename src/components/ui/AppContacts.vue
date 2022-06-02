837691e8a198a22b8662c5bec844e5f923186584
Aleksey <hoock57@gmail.com>
2022-06-02 20:31:13 +0300

    Add icons to contact AppContact component

--- src/components/ui/AppContacts.vue
+++ src/components/ui/AppContacts.vue
@@ -10,14 +10,15 @@
       class="contacts__item item"
     >
       <span class="item__placeholder">
-        {{ contact.type }}
+        {{ contact.name }}
       </span>
       <span class="item__value">
+        <font-awesome-icon :icon="[ icons[contact.type]?.prefix, icons[contact.type]?.icon ]" />
         <a 
           :href="contact.link"
           target="_blank"
         >
-          {{ contact.value }}
+         {{ contact.value }}
         </a>
       </span>
     </div>
@@ -37,6 +38,8 @@
 </template>
 
 <script>
+import icons from '../../icons'
+
 export default {
   props: {
     contacts: {
@@ -46,6 +49,13 @@ export default {
         return {}
       }
     },
-  }
+  },
+
+  data() {
+    return {
+      icons
+    }
+  },
+
 }
 </script>
---
+++ src/icons.js
@@ -0,0 +1,14 @@
+export default {
+  phone: {
+    prefix: 'fa',
+    icon: 'phone'
+  },
+  telegram: {
+    prefix: 'fab',
+    icon: 'telegram'
+  },
+  envelope: {
+    prefix: 'fa',
+    icon: 'envelope'
+  },
+}
