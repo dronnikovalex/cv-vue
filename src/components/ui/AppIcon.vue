3cd3c6bd2f2fa0ba4402d70ec4b072fb5e826c85
Aleksey <hoock57@gmail.com>
2022-06-06 10:03:46 +0300

    Add AppIcon component

---
+++ src/components/ui/AppIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <font-awesome-icon :icon="[ icons[type]?.prefix, icons[type]?.icon ]" />
+</template>
+
+<script>
+import icons from '../../icons'
+
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+
+  data() {
+    return {
+      icons
+    }
+  }
+}
+</script>
