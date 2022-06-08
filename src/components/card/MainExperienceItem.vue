452325a7ad7c0ac35340f118fda04c58ae2e96dc
Aleksey <hoock57@gmail.com>
2022-06-08 19:49:50 +0300

    Add MainExperienceItem component

---
+++ src/components/card/MainExperienceItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="experience__item">
+    <h3 :class="{ experience__last: isLastJob }">{{ experienceItem.name }} | {{ experienceItem.dateFrom }} - {{ experienceItem.dateTo }}</h3>
+    <p class="experience__position">Должность - {{ experienceItem.position }}</p>
+    <p class="experience__description">{{ experienceItem.descripton }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    experienceItem: {
+      type: Object,
+      required: true,
+      default: function() {
+        return {}
+      }
+    }
+  },
+
+  computed: {
+    isLastJob() {
+      return this.experienceItem.last === true ? true : false
+    }
+  }
+}
+</script>
