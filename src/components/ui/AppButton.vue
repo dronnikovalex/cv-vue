2463665dd03ae2cf1da9658b5539f282999bbe95
Aleksey <hoock57@gmail.com>
2022-06-13 16:30:25 +0300

    Add AppButton component

---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button 
+    :disabled="disabled"
+    @click="$emit('action')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    } 
+  }
+}
+</script>
