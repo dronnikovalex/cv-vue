b7e8f6626a6a72658891c9a6d95f04fdd387d1cc
Aleksey <hoock57@gmail.com>
2022-06-13 19:02:15 +0300

    Add faCircleQuestion to project

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import './assets/index.css'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faVk, faTelegram } from "@fortawesome/free-brands-svg-icons";
-import { faHSquare, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faHSquare, faEnvelope, faPhone, faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faGithub, faVk, faHSquare, faEnvelope, faPhone, faTelegram);
+library.add(faGithub, faVk, faHSquare, faEnvelope, faPhone, faTelegram, faCircleQuestion);
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
