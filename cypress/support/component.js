6d1effb77f95f9247913f44a0c5858c904ff877f
Aleksey <hoock57@gmail.com>
2022-06-27 11:06:56 +0300

    Add config to setup.js and connect it to component testing

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -15,15 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import '../../src/assets/index.css'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faGithub, faVk, faTelegram } from "@fortawesome/free-brands-svg-icons";
-import { faHSquare, faEnvelope, faPhone, faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faGithub, faVk, faHSquare, faEnvelope, faPhone, faTelegram, faCircleQuestion);
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+import '../../src/setup'
 
 import { mount } from 'cypress/vue'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -38,5 +30,3 @@ Cypress.Commands.add('mount', (component, options = {}) => {
 
   return mount(component, options)
 })
-// Example use:
-// cy.mount(MyComponent)
--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/index.css'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faGithub, faVk, faTelegram } from "@fortawesome/free-brands-svg-icons";
-import { faHSquare, faEnvelope, faPhone, faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faGithub, faVk, faHSquare, faEnvelope, faPhone, faTelegram, faCircleQuestion);
-
+import './setup.js'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 createApp(App)
