6fee6c5c3110445ae642f64d576cf0c1c4120532
Aleksey <hoock57@gmail.com>
2022-06-27 11:08:56 +0300

    Add start-server-and-test package to project

---
+++ cypress/e2e/card/card.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+class HomePage {
+  static openPage() {
+    cy.visit('/')
+  }
+}
+
+beforeEach(() => {
+  HomePage.openPage()
+})
+
+describe('Test main page', () => {
+  it('should see cv page', () => {
+    cy.location()
+      .its('pathname')
+      .should('eq', '/cv-vue/')
+  })
+})
