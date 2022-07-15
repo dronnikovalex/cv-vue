38fc23a118b3d0fb1c2719a576fca42a4a394930
Aleksey <hoock57@gmail.com>
2022-07-15 17:32:45 +0300

    Add HomePaghe import to card tests

--- cypress/e2e/card/card.cy.js
+++ cypress/e2e/card/card.cy.js
@@ -1,13 +1,41 @@
 /// <reference types="cypress" />
 
+import HomePage from '../../support/PageObjects/HomePage_PO'
+
 beforeEach(() => {
   HomePage.openPage()
 })
 
+
 describe('Test main page', () => {
+
   it('should see cv page', () => {
     cy.location()
       .its('pathname')
       .should('eq', '/cv-vue/')
+  })
+
+  it.only('get contacts', () => {
+
+    const TODO_ITEM_ONE = "Buy Milk"
+    const TODO_ITEM_TWO = "Pay Rent"
+    const TODO_ITEM_THREE = "Pickup Dry Cleaning"
+    
+    const cmd = Cypress.log({
+      name: "create default todos",
+      consoleProps() {
+        return {
+          "Inserted Todos": [TODO_ITEM_ONE, TODO_ITEM_TWO, TODO_ITEM_THREE],
+        }
+      },
+    })  
+
+    cy.get('.contacts__item-md').then(function (listItems)  {
+      cmd.set({el: listItems}).snapshot().end()
+    })
+
+    cy.get('[data-cy="button"]').click().then(function ()  {
+      cmd.snapshot().end()
+    })
   })
 })
