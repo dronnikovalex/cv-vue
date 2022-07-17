a7fbbd7afb92fa6fdbed6c79c3ea96522ce85f90
Aleksey <hoock57@gmail.com>
2022-07-17 19:01:32 +0300

    Add spec to run all component tests

---
+++ src/allSpecs.cy.js
@@ -0,0 +1,13 @@
+import './components/ui/AppAbout.cy'
+import './components/ui/AppButton.cy'
+import './components/ui/AppHeading.cy'
+import './components/ui/AppIcon.cy'
+import './components/ui/AppInput.cy'
+import './components/ui/AppListTitle.cy'
+import './components/ui/AppLoader.cy'
+import './components/ui/AppModal.cy'
+import './components/ui/AppToast.cy'
+import './components/ui/AppAbout.cy'
+
+import './components/links/LinksList.cy'
+import './components/links/LinksListItem.cy'
