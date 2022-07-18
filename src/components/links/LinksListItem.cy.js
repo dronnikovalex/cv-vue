d91cf27e1bf706a940a9238b9dc2160e25f7af59
Aleksey <hoock57@gmail.com>
2022-07-18 10:31:17 +0300

    Add tests for LinksListItem

--- src/components/links/LinksListItem.cy.js
+++ src/components/links/LinksListItem.cy.js
@@ -25,16 +25,30 @@ describe('Test LinksListItem component', () => {
       }
     })
 
-    
     cy.get(listItemSelector, silent).within(silent, () => {
-      
       const { name, value } = link
 
       cy.contains(name).should('exist')
       cy.contains(iconStub).should('exist')
       cy.contains(value).should('exist')
-        
     })
+  })
+
+  it('should not render if no props given', () => {
+    cy.mount(LinksListItem)
+
+    cy.get(listItemSelector, silent)
+      .should('not.exist')
+  })
+
+  it('sholud throw console.warn error if prop "link" not given', () => {
+    cy.stub(window.console, 'warn').as('missedProp')
+
+    cy.mount(LinksListItem)
+
+    cy.get('@missedProp')
+      .should('have.been.called')
+      .and('be.calledWithMatch', 'Missing required prop: "link"')
   })
 
 }) 
