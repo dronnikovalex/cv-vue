55eb8cf147a6c77e787a7e934e97863bb49eeb20
Dronnikov Aleksey <hoock57@gmail.com>
2022-07-26 22:56:08 +0300

    Add tests for TheMainExperience component

--- src/components/card/TheMainExperience.cy.js
+++ src/components/card/TheMainExperience.cy.js
@@ -4,40 +4,50 @@ import TheMainExperience from './TheMainExperience.vue'
 import { createRandomExperienceItem } from '../../../cypress/utils'
 
 describe('Test TheMainExperience component', () => {
-
+  const titleSelector = '[data-cy="card-title"]'
+  const experienceSection = '[data-cy="experience-container"]'
+  
   const experienceItems = [
     createRandomExperienceItem()
   ]
 
   function renderExperienceSection(experienceItems) {
-    if (experienceItems.length) {
+    if (experienceItems && experienceItems.length) {
       return cy.mount(TheMainExperience, {
         props: {
           experience: experienceItems
         }
       })
     }
+
+    return cy.mount(TheMainExperience)
   }
 
   it('should render component', () => {
-    // renderExperienceSection()
+    renderExperienceSection(experienceItems)
 
-    console.log(experienceItems)
+    cy.get(titleSelector)
+      .should('be.visible')
+    cy.get(experienceSection)
+      .should('be.visible')
+      .and('not.be.empty')
   })
 
-  it.skip('should not render experience section if prop "experience" not given', () => {
+  it('should not render experience section if prop "experience" not given', () => {
+    renderExperienceSection()
 
+    cy.get('#experience')
+      .should('not.exist')
   })
 
-  it.skip('sholud throw console.warn error if prop "experience" not given', () => {
+  it('sholud throw console.warn error if prop "experience" not given', () => {
     cy.stub(window.console, 'warn').as('missedProp')
 
-    cy.mount()
+    renderExperienceSection()
 
     cy.get('@missedProp')
       .should('have.been.called')
-      .and('be.calledWithMatch', 'Missing required prop: "isShortTitle"')
-      .and('be.calledWithMatch', 'Missing required prop: "stack"')       
+      .and('be.calledWithMatch', 'Missing required prop: "experience"')
   })
 
 })
