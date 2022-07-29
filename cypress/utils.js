715eabc8aa9670df512a29cf418db6419642347c
Aleksey <hoock57@gmail.com>
2022-07-29 16:06:29 +0300

    Changes:   - remove export from getDates func   - add createRandomStudyItem func   - add createRandomStudyItems func

--- cypress/utils.js
+++ cypress/utils.js
@@ -1,15 +1,23 @@
 import { faker } from '@faker-js/faker'
 
-export function getDates([ fromDate, toDate ]) {
-  return {
-    dateFrom: new Intl.DateTimeFormat('en-US').format(new Date(fromDate)).replace(/\/\d*\//, '/'),
-    dateTo: new Intl.DateTimeFormat('en-US').format(new Date(toDate)).replace(/\/\d*\//, '/')
+function getDates([ fromDate, toDate ], format = 'month') {
+  switch (format) {
+    case 'month':
+      return {
+        dateFrom: new Intl.DateTimeFormat('en-US').format(new Date(fromDate)).replace(/\/\d*\//, '/'),
+        dateTo: new Intl.DateTimeFormat('en-US').format(new Date(toDate)).replace(/\/\d*\//, '/')
+      }
+    case 'year':
+      return {
+        dateFrom: new Intl.DateTimeFormat('en-US').format(new Date(fromDate)).replace(/\d*\/\d*\//, ''),
+        dateTo: new Intl.DateTimeFormat('en-US').format(new Date(toDate)).replace(/\d*\/\d*\//, '')
+      }
   }
 }
 
 export function createRandomExperienceItem() {
   const dates = getDates(faker.date.betweens('2020-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z', 2))
-  const{ dateFrom, dateTo } = dates
+  const { dateFrom, dateTo } = dates
 
   return {
     dateFrom,
@@ -20,4 +28,37 @@ export function createRandomExperienceItem() {
     position: faker.name.jobTitle(),
     website: faker.internet.domainName()
   }
+}
+
+export function createRandomStudyItem() {
+  const dates = getDates(faker.date.betweens('2000-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z', 2), 'year')
+  const { dateFrom, dateTo } = dates
+
+  const randomType = ['university', 'school'][Math.floor(Math.random() * 2)]
+
+  return {
+    dateFrom,
+    dateTo,
+    description: faker.lorem.text(),
+    title: faker.company.companyName(),
+    type: randomType
+  }
+}
+export function createRandomStudyItems(count = 2) {
+  let studyItems = []
+
+  while (count > 0) {
+    studyItems.push(createRandomStudyItem())
+
+    count--
+  }
+
+  if (studyItems.length === 2) {
+    studyItems[0].type = 'university'
+    studyItems[1].type = 'school'
+  } else {
+    studyItems[0].type = 'university'
+  }
+
+  return studyItems
 }
